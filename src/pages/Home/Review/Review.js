94ceb945fcc17e2e0f094797af437838e1bad6f0
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-11-23 13:34:35 +0600

    review design change

--- src/pages/Home/Review/Review.js
+++ src/pages/Home/Review/Review.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Grid, Rating, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import StarRatings from 'react-star-ratings';
 
@@ -9,16 +9,16 @@ const Review = (props) => {
     const { name, review, star, img } = props.review;
     return (
         <Grid item xs={4} sm={4} md={4}>
-            <Card sx={{ minWidth: 275, height: '150px', border: 0, boxShadow: 2 }}>
+            <Card sx={{ minWidth: 275, height: '145px', border: 0, boxShadow: 2 }}>
                 <Box style={{display:'flex', justifyContent:'spaceBetween', alignItems:'center'}}>
-                    <Box style={{textAlign: 'left', marginTop: '10px', marginBottom: '10px' }}>
+                    <Box style={{textAlign: 'left', marginTop: '10px', marginBottom: '10px', border:'2px solid gray', padding:'10px', borderRadius:'25px 0 25px 0', marginLeft:'10px' }}>
                         <CardMedia
                             component="img"
-                            style={{ width: '50px', borderRadius: '50%', marginLeft:'10px' }}
+                            style={{ width: '50px', borderRadius: '5%' }}
                             image={img}
                             alt="your img"
                         />
-                        <Box sx={{ ml: 2 }}>
+                        <Box sx={{ }}>
                             <Typography variant="body2" sx={{ fontSize: 14, color: '#8A513D', fontWeight: 400 }} >
                                 {name}
                             </Typography>
@@ -32,11 +32,7 @@ const Review = (props) => {
                                 <i style={{ color: '#8A513D', margin: '5px' }} class="fas fa-quote-right fa-1x"></i>
                             </Typography>
                         </CardContent>
-                        <StarRatings
-                            rating={parseFloat(star)}
-                            starDimension="15px"
-                            starSpacing="10px"
-                        />
+                        <Rating name="read-only" value={parseFloat(star)} readOnly />
                     </Box>
                 </Box>
             </Card>
